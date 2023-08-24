285091189d484fbff6d063d66f266d7c402de4a7
Vino wang (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-wangvino@microsoft.com>
2023-08-24 09:01:32 +0800

    we

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+sfsf
+    sfsfsfsfsfsfv
+    sfsf
+    sfsf
+    sfsf
+    sfsf
+    sfsf
+    sfsf
+
+
+    sfsf
 
